a2fb97917ccb61d16713473ce954a01d43a96079
Anthonyz28 <anthony.z28@hotmail.com>
2021-02-11 18:16:54 -0600

    inquirer

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.lenght);
+//const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+//const generatePage = require('.src/page-template');
 
-const generatePage = require('./src/page-template');
+//const pageHTML = generatePage(name, github);
 
-  
+//fs.writeFile('index.html', generatePage(name, github), err => {
+   // if (err) throw err;
+
+   // console.log('Portfolio complete! Check out index.html to see the output!')
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            messages: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
--- src/page-template.js
+++ src/page-template.js
@@ -16,9 +16,5 @@ const generatePage = (userName, githubName) => {
     </html>
     `;
   };
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+  
   modeule.exports = generatePage;
