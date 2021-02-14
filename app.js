178dfa641d4d8798adca7f9c73be249a3a19f132
Anthonyz28 <anthony.z28@hotmail.com>
2021-02-14 10:49:15 -0600

    adanced html

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const inquirer = require('inquirer');
 
-//const fs = require('fs');
+const fs = require('fs');
 
-//const generatePage = require('.src/page-template');
+const generatePage = require('./src/page-template.js');
 
 //const pageHTML = generatePage(name, github);
 
-//fs.writeFile('index.html', generatePage(name, github), err => {
-   // if (err) throw err;
+//fs.writeFile('index.html', generatePage(), err => {
+  //  if (err) throw err;
 
    // console.log('Portfolio complete! Check out index.html to see the output!')
-// });
+ //});
 const promptUser = () => {
 
 
@@ -148,5 +148,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(projectAnswers => {
-        console.log(projectAnswers);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!')
+            });
     });
